94d3e183e60286162bf2bb8432b2d12478ec9781
mottafoka <guiap.mota@hotmail.com>
2019-09-26 18:04:06 -0300

    1.0.2 | QRCode update

--- client/src/main/java/application/Run.java
+++ client/src/main/java/application/Run.java
@@ -1,10 +1,11 @@
 package application;
+
 // import helpers.Initialize;
 import service.QRCodeService;
 
 public class Run {
-    public static void main(String[] args) {
-        // Initialize.start();
-        new QRCodeService().fFrame(12,"9203840298439");
-    }
+	public static void main(String[] args) {
+		// Initialize.start();
+		new QRCodeService().fFrame(12, "9203840298439");
+	}
 }
--- client/src/main/java/service/QRCodeService.java
+++ client/src/main/java/service/QRCodeService.java
@@ -1,28 +1,31 @@
 package service;
 
 import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.BarcodeQRCode;
 import java.awt.Color;
 
 public class QRCodeService extends JFrame {
 	private static final long serialVersionUID = 9022142288376442819L;
 
 	JFrame frame = new JFrame();
+	Image codeQrImage = null;
 
 	public void fFrame(Integer id, String serial) {
 
 		frame.setSize(500, 500);
 		frame.setTitle("QRCode do computador N° " + id);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.getContentPane().setBackground(Color.white);
 
-		JLabel qrcode = new JLabel();
-		qrcode.setIcon(new ImageIcon("http://s.glbimg.com/jo/g1/f/original/2011/05/17/qrcode.jpg"));
+		BarcodeQRCode bcQRCode = new BarcodeQRCode(serial, 500, 500, null);
+
+		JLabel qrcode = new JLabel(bcQRCode.toString());
+		qrcode.setSize(10, 10);
 		qrcode.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		qrcode.setBounds(10,10,10,10);
-		qrcode.setOpaque(true);
 		frame.add(qrcode);
 
 		frame.setVisible(true);
