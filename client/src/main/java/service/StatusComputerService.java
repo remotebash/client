ff3f5f19ef8bf2b8e479681c313363dd129929cc
mateuscovos <mateus.covos@gmail.com>
2019-11-03 21:58:26 -0300

    adicionado integração com computador online

--- client/src/main/java/service/StatusComputerService.java
+++ client/src/main/java/service/StatusComputerService.java
@@ -1,6 +1,8 @@
 package service;
 
+import helpers.ComputerHelper;
 import helpers.ConstantsHelper;
+import helpers.HttpHelper;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -8,7 +10,11 @@ import java.util.TimerTask;
 public class StatusComputerService {
 
 
-    private static void setComputerOnline(){ System.out.println(ConstantsHelper.URL_MICROSERVICE_COMPUTER_ONLINE); }
+    private static void setComputerOnline(){
+    	String jsonObject = "{id: \""+ComputerHelper.getComputer().getIdComputer()+"\"}";
+    	HttpHelper http = new HttpHelper();
+    	http.requestPost(ConstantsHelper.URL_MICROSERVICE_COMPUTER_ONLINE, jsonObject);
+    }
 
     public static void computerOnline(){
         Timer timer = new Timer();
